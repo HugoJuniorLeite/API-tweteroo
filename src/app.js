0527fb9d0354cbe215fb5da80209b3c636b73a96
Hugo Junior <hjunioreng@gmail.com>
2023-01-09 13:08:03 -0300

    corrigindo validações

--- src/app.js
+++ src/app.js
@@ -24,6 +24,10 @@ app.post("/sign-up",(req,res)=>{
 
 app.post("/tweets", (req,res)=>{    
     const { username, tweet} = req.body;
+     if(!username){
+             return res.status(401).send("UNAUTHORIZED")
+         };
+
     const use =req.headers.user;
     
     const message = 
@@ -32,12 +36,10 @@ app.post("/tweets", (req,res)=>{
             avatar: useravatar,
             tweet 
         };
-        if( !tweet || typeof tweet !== "string" || (!username && !use) ){
+        if( !tweet || typeof tweet !== "string" || !use ){
             return res.status(400).send("Todos os campos são obrigatórios!")
         };
-        // if( !use && !username){
-            // return res.status(401).send("UNAUTHORIZED")
-        // };
+        
     tweets.unshift(message);
     return res.status(201).send("CREATED")
 });
